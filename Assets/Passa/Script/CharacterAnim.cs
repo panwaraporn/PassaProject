6de7cce9e203ad6fd027c71120c96cf88c7c24de
panwaraporn <30980249+panwaraporn@users.noreply.github.com>
2019-04-06 14:32:44 +0700

    commit 6
    in morning

--- Assets/Passa/Script/CharacterAnim.cs
+++ Assets/Passa/Script/CharacterAnim.cs
@@ -6,11 +6,13 @@ public class CharacterAnim : MonoBehaviour
 {
     public Animator anim;
     int Jump = Animator.StringToHash("Jump");
+    public Rigidbody2D rigid;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rigid.AddForce(new Vector3(0, 0, 0));
     }
 
     // Update is called once per frame
@@ -24,9 +26,14 @@ public class CharacterAnim : MonoBehaviour
         {
             anim.SetBool("isRunning", false);
         }
+
+
         if (Input.GetKeyDown(KeyCode.Space)){
             anim.SetTrigger(Jump);
          }
+
+
+
          
         /*
         float move = Input.GetAxis("Vertical");
