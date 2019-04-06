d0dd2a55619b48098ef82022246baa6a539266e1
panwaraporn <30980249+panwaraporn@users.noreply.github.com>
2019-04-06 14:47:25 +0700

    commit7
    delete git on unity

--- Assets/Passa/Script/CharacterAnim.cs
+++ Assets/Passa/Script/CharacterAnim.cs
@@ -32,17 +32,10 @@ public class CharacterAnim : MonoBehaviour
             anim.SetTrigger(Jump);
          }
 
+    }
 
-
-         
-        /*
-        float move = Input.GetAxis("Vertical");
-        anim.SetFloat("Speed", move);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger(Jump);
-        }
-        */
+    void FixedUpdate()
+    {
+        
     }
 }
